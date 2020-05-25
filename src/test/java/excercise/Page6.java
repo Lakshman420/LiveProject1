0dc22f725051ded3b722ec115fb82e98c666d4da
Lakshman420 <lakshman.chanti20@gmail.com>
2020-05-25 11:04:18 +0530

    Page5 added in develop branch

---
+++ src/test/java/excercise/Page6.java
@@ -0,0 +1,34 @@
+package excercise;
+
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class Page6 {
+	@BeforeTest
+	public void setUpBT() {
+		System.out.println("Before Test Page4");
+	}
+	
+	@BeforeMethod
+	public void setUpBM() {
+		System.out.println("Before Method Page4");
+	}
+	@Test(groups={"Regression","Functional"})
+	public void testexecution() {
+		System.out.println("Test Page4");
+	}
+	
+	@AfterMethod
+	public void tearDownBM() {
+		System.out.println("After Method Page4");
+	}
+	
+	@AfterTest
+	public void tearDownAT() {
+		System.out.println("After Test Page4");
+	}
+
+}
